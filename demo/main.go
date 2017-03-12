aaf8e427768ddcc80a026e28d196c4ff1961f149
qianlnk <qianlnk@qianlnk-MAC.local>
2017-03-13 01:49:04 +0800

    add demo

--- demo/main.go
+++ demo/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"time"
+
 	"github.com/qianlnk/log"
 )
 
+//run it as: nohub ./demo > a &
 func main() {
-	log.Info("test")
+	for {
+		log.Info("test")
+		log.Fields{
+			"name": "qianlnk",
+			"age":  "27",
+		}.Info("test fields")
+		log.Error("test error")
+		time.Sleep(time.Second * 2)
+	}
 }
